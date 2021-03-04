2979be138a99e47dae5c6e0ce80362ddb18459e4
Shubham <16763337+shubham-v@users.noreply.github.com>
2021-03-04 21:10:59 +0530

    af

---
+++ shors.py
@@ -0,0 +1,15 @@
+# https://www.quantiki.org/wiki/shors-factoring-algorithm
+from qiskit.aqua.algorithms import Shor
+from qiskit.aqua import QuantumInstance
+from qiskit import Aer
+
+key = 21
+base = 2
+
+backend = Aer.get_backend('qasm_simulator')
+
+qi = QuantumInstance(backend=backend, shots=1024)
+
+shors = Shor(N=key, a=base, quantum_instance=qi)
+results=shors.run()
+print(results['factors'])
